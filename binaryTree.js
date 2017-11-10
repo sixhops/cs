758e96114aca0a8845b5ad9effbcfbc53f809c00
Steven Peters <steve@Jennas-MacBook-Air.local>
2017-11-09 22:42:55 -0800

    Finishing binary search tree in js.

--- binaryTree.js
+++ binaryTree.js
@@ -1,13 +1,9 @@
-
 class TreeNode {
   constructor(data) {
     this.leftChild = null;
     this.rightChild = null;
     this.data = data;
   }
-  // getData() {
-  //   return this.data;
-  // }
 }
 
 class BinarySearchTree {
@@ -15,29 +11,85 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  // Insert a new node with given data
   insert(node, data) {
     // If the tree is empty, return a new node
-    if (node === null) return new TreeNode(data);
+    //  This should only ever occur once!
+    if (this.root === null) {
+      this.root = new TreeNode(data);
+      return node;
+    }
 
     // Otherwise, recurse down the tree
-    if (data < node.data) {
-      node.leftChild = insert(node.leftChild, data);
+    if (data < node.data && node.leftChild === null) {
+      // Set left child directly...
+      node.leftChild = new TreeNode(data);
+    } else if (data < node.data) {
+      // ...or recurse if its already set.
+      node.leftChild = this.insert(node.leftChild, data);
+    } else if (data > node.data && node.rightChild === null) {
+      // Set the right child directly...
+      node.rightChild = new TreeNode(data);
     } else if (data > node.data) {
-      node.rightChild = insert(node.rightChild, data);
+      // ...or recurse if its already set.
+      node.rightChild = this.insert(node.rightChild, data);
     }
-    // return the (unchanged) node pointer
+    // return the node pointer
     return node;
   }
 
-  inOrderTraversal() {
+  preOrderTraversal(node) {
+    // root, left, right
+    console.log(node.data);
+    if (node) {
+      if (node.leftChild) {
+        this.preOrderTraversal(node.leftChild);
+      }
+      if (node.rightChild) {
+        this.preOrderTraversal(node.rightChild);
+      }
+    }
+  }
+
+  inOrderTraversal(node) {
     // left, root, right
-    
+    if (node) {
+      if (node.leftChild) {
+        this.inOrderTraversal(node.leftChild);
+      }
+      console.log(node.data);
+      if (node.rightChild) {
+        this.inOrderTraversal(node.rightChild);
+      }
+    }
+  }
+
+  postOrderTraversal(node) {
+    // left, right, root
+    if (node) {
+      if (node.leftChild) {
+        this.postOrderTraversal(node.leftChild);
+      }
+      if (node.rightChild) {
+        this.postOrderTraversal(node.rightChild);
+      }
+      console.log(node.data);
+    }
   }
 }
 
 var bst = new BinarySearchTree();
 
-bst.insert(100);
-bst.insert(40);
-bst.insert(175);
+bst.insert(bst.root, 100);
+bst.insert(bst.root, 40);
+bst.insert(bst.root, 175);
+bst.insert(bst.root, 53);
+bst.insert(bst.root, 201);
+bst.insert(bst.root, 16);
+bst.insert(bst.root, 151);
+
+console.log("In order: ");
+bst.inOrderTraversal(bst.root);
+console.log("Pre order: ");
+bst.preOrderTraversal(bst.root);
+console.log("Post order: ")
+bst.postOrderTraversal(bst.root);
